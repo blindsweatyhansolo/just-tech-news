69bc8788da06581655ac8a381db4d07fcf4d08fe
J Semidey <mtsemidey@gmail.com>
2022-05-17 15:51:08 -0400

    set up single-post page, with comment/upvote functionality depending on session status

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -90,29 +90,35 @@ router.get('/:id', (req, res) => {
 
 // POST create new post
 router.post('/', (req, res) => {
-    Post.create({
-        title: req.body.title,
-        post_url: req.body.post_url,
-        user_id: req.body.user_id
-    })
-      .then(dbPostData => res.json(dbPostData))
-      .catch(err => {
-          console.log(err);
-          res.status(500).json(err);
-      });
+    // check if session exists
+    if (req.session) {
+        Post.create({
+            title: req.body.title,
+            post_url: req.body.post_url,
+            user_id: req.session.user_id
+        })
+          .then(dbPostData => res.json(dbPostData))
+          .catch(err => {
+              console.log(err);
+              res.status(500).json(err);
+          });
+    }
 });
 
 // PUT /api/posts/upvote
 // voting on a post is actually updating a post's data
 // must be defined before the put /:id route
 router.put('/upvote', (req, res) => {
-    // custom static method for upvote() created in models/Post.js
-    Post.upvote(req.body, { Vote })
-      .then(updatedPostData => res.json(updatedPostData))
-      .catch(err => {
-          console.log(err);
-          res.status(400).json(err);
-      });
+    // check if session exists
+    if (req.session) {
+        // pass session id along with all destructured properties on req.body
+        Post.upvote({ ...req.body, user_id: req.session.user_id }, { Vote, Comment, User })
+        .then(updatedVoteData => res.json(updatedVoteData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+    }
 });
 
 // PUT update a post's title
--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,7 +3,7 @@ const router = require('express').Router();
 const sequelize = require('../config/connection');
 const {Post, User, Comment, Vote } = require('../models');
 
-// root route
+// root route (gets all posts for homepage)
 router.get('/', (req, res) => {
     console.log(req.session);
 
@@ -36,7 +36,59 @@ router.get('/', (req, res) => {
 
         // res.render used to specify which template to use (first arg: homepage.handlebars)
         // can accept an object that includes all the data to be passed into the template
-        res.render('homepage', { posts });
+        res.render('homepage', { 
+            posts,
+            loggedIn: req.session.loggedIn
+        });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+// single-post route
+router.get('/post/:id', (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: [
+            'id',
+            'post_url',
+            'title',
+            'created_at',
+            [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                        model: User,
+                        attributes: ['username']
+                    }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+    .then(dbPostData => {
+        if (!dbPostData) {
+            res.status(404).json({message: 'No post found with this id!'});
+            return;
+        }
+
+        // serialize the data
+        const post = dbPostData.get({ plain: true });
+
+        // pass data to template, second variable is set as loggedIn session variable
+        res.render('single-post', { 
+            post,
+            loggedIn: req.session.loggedIn
+        })
     })
     .catch(err => {
         console.log(err);
